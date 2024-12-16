7955854718bf1ac2df852b4a81db2a74c209fa57
Idan Benik <idan.benik@gmail.com>
2024-12-16 12:22:34 +0100

    solved dark mode

--- js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events/js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkModeBtn = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeBtn = document.querySelector('[data-js="light-mode-button"]');
+const toggleBtn = document.querySelector('[data-js="toggle-button"]');
+
+darkModeBtn.addEventListener('click', () => {
+   bodyElement.classList.add('dark');
+   console.log('add dark class');
+});
+
+lightModeBtn.addEventListener('click', () => {
+   bodyElement.classList.remove('dark');
+   console.log('remove dark class');
+});
+
+toggleBtn.addEventListener('click', () => {
+   bodyElement.classList.toggle('dark');
+   console.log('toggle dark mode');
+});
