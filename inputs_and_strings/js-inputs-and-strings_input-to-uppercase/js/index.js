e4c3ede75b8c2d3ae100f0494b8e5b0f7450734b
Idan Benik <idan.benik@gmail.com>
2024-12-18 15:02:19 +0100

    toUpper

--- inputs_and_strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ inputs_and_strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+
+const input = document.querySelector('[data-js="first-input"]');
+const button = document.querySelector('[data-js="button-uppercase"]');
+
+button.addEventListener('click', () => {
+   const newValue = input.value.toUpperCase();
+   input.value = newValue;
+});
