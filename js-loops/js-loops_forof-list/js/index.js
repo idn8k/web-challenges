ff3fc362978dfcca65f5b4d578dc9d63d248c507
Idan Benik <idan.benik@gmail.com>
2024-12-20 15:35:13 +0100

    list - done

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -2,19 +2,25 @@ console.clear();
 
 const main = document.querySelector('[data-js="main"]');
 
-const ol = document.createElement("ol");
+const ol = document.createElement('ol');
 main.append(ol);
 
 const programmingLanguages = [
-  "JavaScript",
-  "Python",
-  "Java",
-  "C#",
-  "C++",
-  "PHP",
-  "Ruby",
+   'JavaScript',
+   'Python',
+   'Java',
+   'C#',
+   'C++',
+   'PHP',
+   'Ruby',
 ];
 
 // --v-- write or modify code below this line --v--
+
+for (let item of programmingLanguages) {
+   const li = document.createElement('li');
+   li.textContent = item;
+   ol.append(li);
+}
 
 // --^-- write or modify code above this line --^--
