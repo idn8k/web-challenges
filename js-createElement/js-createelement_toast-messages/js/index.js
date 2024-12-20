d489a8e83066d01a160498f8f0f952519111d43b
Idan Benik <idan.benik@gmail.com>
2024-12-20 11:42:34 +0100

    toast - done

--- js-createElement/js-createelement_toast-messages/js/index.js
+++ js-createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,18 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+let count = 0;
+
+addButton.addEventListener('click', () => {
+   // Exercise: Append a new entry to the toast messages container
+   const counter = count++;
+   const newLi = document.createElement('li');
+   newLi.setAttribute('class', 'toast-container__message');
+   newLi.innerText = `I'm toast no.${counter}`;
+
+   toastContainer.append(newLi);
 });
 
-clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+clearButton.addEventListener('click', () => {
+   // Exercise: Clear the stack of toast messages
 });
