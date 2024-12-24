dfe682c501971e67a0e147b917cfbca5de4faadd
Idan Benik <idan.benik@gmail.com>
2024-12-24 15:44:57 +0100

    pizza - done

--- js-functions/js-functions_pizza-analyzer/js/index.js
+++ js-functions/js-functions_pizza-analyzer/js/index.js
@@ -9,29 +9,54 @@ const gainOutput = document.querySelector('[data-js="output"]');
 
 const output = document.querySelector('[data-js="output"]');
 
-pizzaInput1.addEventListener('input', () => {
-   // write your code here
-});
+let pizzaSize1 = pizzaInput1.value;
+let pizzaSize2 = pizzaInput2.value;
 
-pizzaInput2.addEventListener('input', () => {
+pizzaInput1.addEventListener('input', (e) => {
    // write your code here
+   pizzaSize1 = Number(e.target.value);
+   calculatePizzaGain(pizzaSize1, pizzaSize2);
+   updatePizzaDisplay(pizzaSize1, pizza1);
+   updateOutputColor(pizzaSize1, pizzaSize2);
 });
-
+pizzaInput2.addEventListener('input', (e) => {
+   // write your code here
+   pizzaSize2 = Number(e.target.value);
+   calculatePizzaGain(pizzaSize1, pizzaSize2);
+   updatePizzaDisplay(pizzaSize2, pizza2);
+   updateOutputColor(pizzaSize1, pizzaSize2);
+});
 // Task 1
 // define the function calculatePizzaGain here
-const diameter1 = 24;
-const diameter2 = 35;
 
 function calculatePizzaGain(diameter1, diameter2) {
+   if (diameter1 <= 0 || diameter2 <= 0) return;
    const area1 = Math.PI * (diameter1 / 2) ** 2;
    const area2 = Math.PI * (diameter2 / 2) ** 2;
    const pizzaGain = Math.round(((area2 - area1) / area1) * 100);
-   output.innerHTML = pizzaGain;
+   output.textContent = pizzaGain;
 }
-calculatePizzaGain(diameter1, diameter2);
 
 // Task 2
 // define the function updatePizzaDisplay here
+function updatePizzaDisplay(newSize, pizzaElement) {
+   const size = (newSize / 24) * 100;
+   pizzaElement.style.width = size + 'px';
+}
 
 // Task 3
 // define the function updateOutputColor here
+
+function updateOutputColor(pizza_one, pizza_two) {
+   if (pizza_two < pizza_one) {
+      outputSection.style.backgroundColor = 'rgb(226, 51, 109)';
+   } else {
+      outputSection.style.backgroundColor = 'rgb(92, 220, 118)';
+   }
+}
+
+// // Get the value of a CSS variable
+// const root = document.documentElement;
+// const mainColor = getComputedStyle(root).getPropertyValue('--main-color').trim();
+
+// console.log(mainColor); // Outputs: #3498db
