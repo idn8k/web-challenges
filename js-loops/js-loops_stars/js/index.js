b47da1ef2f104748b023e65b787b80586d14cf3f
Idan Benik <idan.benik@gmail.com>
2024-12-31 23:31:10 +0100

    Update changes

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,20 +2,14 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
-   // Reset the star container before re-rendering stars
+function renderStars(fillesStars) {
    starContainer.innerHTML = '';
-   const star = document.createElement('img');
-   star.src = '../assets/star-empty.svg';
-
-   // --v-- write or modify code below this line --v--
 
    for (let i = 1; i <= 5; i++) {
-      console.log('star');
+      const star = document.createElement('img');
+      star.src = '../assets/star-empty.svg';
       starContainer.append(star);
    }
-
-   // --^-- write or modify code above this line --^--
 }
 
 renderStars();
