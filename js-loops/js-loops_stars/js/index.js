dd72778c2e0536181439df7f2f885e0adc26bc08
Idan Benik <idan.benik@gmail.com>
2025-01-01 00:18:24 +0100

    stars - done

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,14 +2,35 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars(fillesStars) {
+function renderStars(filledStars) {
    starContainer.innerHTML = '';
-
-   for (let i = 1; i <= 5; i++) {
-      const star = document.createElement('img');
-      star.src = '../assets/star-empty.svg';
-      starContainer.append(star);
+   if (filledStars) {
+      for (let i = 1; i <= filledStars; i++) {
+         const star = document.createElement('img');
+         star.src = '../assets/star-filled.svg';
+         star.addEventListener('click', () => {
+            console.log('click');
+         });
+         starContainer.append(star);
+      }
+      for (let i = 1; i <= 5 - filledStars; i++) {
+         const star = document.createElement('img');
+         star.src = '../assets/star-empty.svg';
+         star.addEventListener('click', () => {
+            console.log('click');
+         });
+         starContainer.append(star);
+      }
+   } else {
+      for (let i = 1; i <= 5; i++) {
+         const star = document.createElement('img');
+         star.src = '../assets/star-empty.svg';
+         star.addEventListener('click', () => {
+            console.log('click');
+         });
+         starContainer.append(star);
+      }
    }
 }
 
-renderStars();
+renderStars(3);
