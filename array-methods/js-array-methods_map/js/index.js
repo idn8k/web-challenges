a7928aa9049805c2979cc7fa9a3d03fcf6e42349
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-07 16:18:53 +0100

    map done

--- array-methods/js-array-methods_map/js/index.js
+++ array-methods/js-array-methods_map/js/index.js
@@ -1,33 +1,44 @@
 console.clear();
 
 const cards = [
-  {
-    id: "1",
-    isBookmarked: false,
-    question: "How often can I use <header>?",
-    answer: "As often as you like.",
-    tags: ["html", "elements", "basic"],
-  },
-  {
-    id: "2",
-    isBookmarked: false,
-    question: "How often can I use <aside>?",
-    answer: "As often as you like.",
-    tags: ["html", "elements", "basic"],
-  },
-  {
-    id: "3",
-    isBookmarked: true,
-    question: "On which types can I use destructuring assignment?",
-    answer: "On Objects and Arrays",
-    tags: ["js", "next", "advanced"],
-  },
+   {
+      id: '1',
+      isBookmarked: false,
+      question: 'How often can I use <header>?',
+      answer: 'As often as you like.',
+      tags: ['html', 'elements', 'basic'],
+   },
+   {
+      id: '2',
+      isBookmarked: false,
+      question: 'How often can I use <aside>?',
+      answer: 'As often as you like.',
+      tags: ['html', 'elements', 'basic'],
+   },
+   {
+      id: '3',
+      isBookmarked: true,
+      question: 'On which types can I use destructuring assignment?',
+      answer: 'On Objects and Arrays',
+      tags: ['js', 'next', 'advanced'],
+   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => card.answer.toLowerCase());
+console.log(lowerCaseAnswers);
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+   const question = card.question;
+   const answer = card.answer;
+   return `${question} ${answer}`;
+}); // ["How often can I use <header>? - As often as you like.", ...]
+console.log(questionsAndAnswersTogether);
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+const questionAndAnswer = cards.map((card) => {
+   const question = card.question;
+   const answer = card.answer;
+   return { question: question, answer: answer };
+}); // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+console.log(questionAndAnswer);
 
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
