998aedb2854576f44beaed297674380f3514e447
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-08 16:53:10 +0100

    console - done

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -3,8 +3,23 @@ console.clear();
 const url = 'https://swapi.py4e.com/api/people';
 
 async function fetchData() {
-   const response = await fetch(url);
-   console.log(response);
+   try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(response);
+
+      if (!response.ok) {
+         console.log('Something went wrong');
+      }
+
+      const characters = data.results;
+      const [r2d2] = characters.filter(
+         (character) => character.name === 'R2-D2'
+      );
+      console.log('R2-D2 eye color:', r2d2.eye_color);
+   } catch (error) {
+      console.log('ERROR', error);
+   }
 }
 
 fetchData();
