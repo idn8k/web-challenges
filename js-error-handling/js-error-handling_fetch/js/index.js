558a62fc8373b49d29d21cb4b73ebfc3be8eb708
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-09 15:36:25 +0100

    fetch-done

--- js-error-handling/js-error-handling_fetch/js/index.js
+++ js-error-handling/js-error-handling_fetch/js/index.js
@@ -5,40 +5,49 @@ const userElement = document.querySelector("[data-js='user']");
 const errorElement = document.querySelector("[data-js='error']");
 
 async function fetchUserData(url) {
-  try {
-    const response = await fetch(url);
+   try {
+      const response = await fetch(url);
+      const contType = response.headers.get('content-type');
 
-    return await response.json();
-  } catch (error) {
-    return { error: error.message };
-  }
+      if (!response.ok) {
+         throw new Error(`${response.status} : not found...`);
+      } else if (!contType.includes('json')) {
+         throw new Error(
+            `Unexpected type: ${contType}.
+             Expected a "json" type.`
+         );
+      }
+      return await response.json(`Unexpected type: ${contType}`);
+   } catch (error) {
+      console.error('error:', error.message);
+      return { error: error.message };
+   }
 }
 
 const endpoints = [
-  { name: "User 1", url: "https://reqres.in/api/users/1" },
-  { name: "User 2", url: "https://reqres.in/api/users/2" },
-  { name: "User 99", url: "https://reqres.in/api/users/99" },
-  { name: "Invalid API link", url: "https://reqres.in" },
+   { name: 'User 1', url: 'https://reqres.in/api/users/1' },
+   { name: 'User 2', url: 'https://reqres.in/api/users/2' },
+   { name: 'User 99', url: 'https://reqres.in/api/users/99' },
+   { name: 'Invalid API link', url: 'https://reqres.in' },
 ];
 
 endpoints.forEach((endpoint) => {
-  const button = document.createElement("button");
-  button.textContent = endpoint.name;
-  actionsElement.append(button);
+   const button = document.createElement('button');
+   button.textContent = endpoint.name;
+   actionsElement.append(button);
 
-  button.addEventListener("click", async () => {
-    const result = await fetchUserData(endpoint.url);
-
-    if (result.error) {
-      errorElement.textContent = result.error;
-      userElement.innerHTML = "No user data available.";
-    } else {
-      const user = result.data;
-      userElement.innerHTML = `
+   button.addEventListener('click', async () => {
+      const result = await fetchUserData(endpoint.url);
+      if (result.error) {
+         errorElement.textContent = result.error;
+         userElement.innerHTML = 'No user data available.';
+      } else {
+         const user = result.data;
+         userElement.innerHTML = `
       <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}" class="user__image"/>
       <h2>${user.first_name} ${user.last_name}</h2>
       `;
-      errorElement.textContent = "";
-    }
-  });
+         errorElement.textContent = '';
+      }
+   });
 });
