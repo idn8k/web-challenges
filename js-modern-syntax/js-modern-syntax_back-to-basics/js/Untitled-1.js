9c6ddb5cd79966817dcc72fce12c8c72e3dc9065
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-11 21:12:42 +0100

    wip

--- js-modern-syntax/js-modern-syntax_back-to-basics/js/Untitled-1.js
+++ js-modern-syntax/js-modern-syntax_back-to-basics/js/Untitled-1.js
@@ -3,18 +3,30 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
+// export const getNameAndCountryCOPY = ({ name, country }) => [name, country];
+
+// export const getRelocatedCityCOPY = (
+//    city1,
+//    city2 = { name: 'Berlin', country: 'Germany' }
+// ) => {
+//    const [, country] = getNameAndCountry(city2);
+//    return {
+//       ...city1,
+//       country,
+//    };
+// };
+
 export const getNameAndCountry = (name, country) => [name, country];
 
 export const getRelocatedCity = (
    city1,
    city2 = { name: 'Berlin', country: 'Germany' }
 ) => {
-   console.log(city2);
    const result = getNameAndCountry(city2.name, city2.country);
-   // const country = result[1];
+   const country = result[1];
 
    return {
       city1,
-      result,
+      country,
    };
 };
