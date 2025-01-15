5dcba618084ae9c4f818c7958b4175689a5afadf
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-15 11:21:18 +0100

    greeting - done

--- react/react-props/react-props_greeting/src/App.jsx
+++ react/react-props/react-props_greeting/src/App.jsx
@@ -1,3 +1,13 @@
+function Greeting({ name }) {
+   if (name === 'Klaus' || name === 'Gimena') {
+      name = 'Coach';
+   } else {
+      name = 'stranger';
+   }
+
+   return <h1>Hello, {name}!</h1>;
+}
+
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+   return <Greeting name="Klaus" />;
 }
