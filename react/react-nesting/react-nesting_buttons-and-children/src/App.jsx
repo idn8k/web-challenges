09e28f31b540e6421ff24da124d375c20a7b6c5f
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-15 15:09:31 +0100

    btn_nad_children - done

--- react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react/react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -1,21 +1,24 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
 
 export default function App() {
-  return (
-    <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </main>
-  );
+   return (
+      <main>
+         <Button>Click 1</Button>
+         <Button>Click 2</Button>
+         <Button>Click 3</Button>
+         <Button>Click 4</Button>
+      </main>
+   );
 }
 
-function Button() {
-  return (
-    <button className="button" type="button">
-      Click me!
-    </button>
-  );
+function Button({ children }) {
+   return (
+      <button
+         className="button"
+         type="button"
+      >
+         {children}
+      </button>
+   );
 }
