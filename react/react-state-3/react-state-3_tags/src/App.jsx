d28aac25ef14adbfa66417d35cb593d81a6eba66
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-20 16:01:16 +0100

    tags - done

--- react/react-state-3/react-state-3_tags/src/App.jsx
+++ react/react-state-3/react-state-3_tags/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
-import "./styles.css";
-import Form from "./components/Form";
-import List from "./components/List";
+import { useState } from 'react';
+import './styles.css';
+import Form from './components/Form';
+import List from './components/List';
 
 export default function App() {
-  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+   const [tags, setTags] = useState(['JavaScript', 'React', 'CSS', 'HTML']);
 
-  return (
-    <main className="app">
-      <Form />
-      <List tags={tags} />
-    </main>
-  );
+   function handleAddTag(newTag) {
+      setTags([...tags, newTag]);
+   }
+
+   function handleDeleteTag(selectedTag) {
+      const newTags = tags.filter((tag) => tag !== selectedTag);
+      setTags(newTags);
+   }
+
+   return (
+      <main className="app">
+         <Form onAddTag={handleAddTag} />
+         <List
+            tags={tags}
+            onDeleteTag={handleDeleteTag}
+         />
+      </main>
+   );
 }
