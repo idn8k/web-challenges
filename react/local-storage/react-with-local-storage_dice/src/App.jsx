6513e1d854e4f0c686cc1f293916614a6b7c3c87
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-01-21 16:07:13 +0100

    wip

--- react/local-storage/react-with-local-storage_dice/src/App.jsx
+++ react/local-storage/react-with-local-storage_dice/src/App.jsx
@@ -1,16 +1,29 @@
-import { D6Button } from "./components/D6Button";
-import History from "./components/History";
-import "./styles.css";
+import { useState } from 'react';
+import { getD6Roll } from './utils';
+
+import { D6Button } from './components/D6Button';
+import History from './components/History';
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="app">
-      <main className="app__main">
-        <D6Button />
-      </main>
-      <aside className="app__aside">
-        <History rolls={[]} />
-      </aside>
-    </div>
-  );
+   const [rolls, setRolls] = useState([]);
+
+   const handleRoll = () => {
+      setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+   };
+   console.log(rolls);
+
+   return (
+      <div className="app">
+         <main className="app__main">
+            <D6Button
+               onRoll={handleRoll}
+               rolls={rolls}
+            />
+         </main>
+         <aside className="app__aside">
+            <History rolls={[]} />
+         </aside>
+      </div>
+   );
 }
