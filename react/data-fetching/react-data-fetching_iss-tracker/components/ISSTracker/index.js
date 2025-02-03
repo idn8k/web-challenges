5f54bc566acfb5dfa7f930fa94d7d54563abdf17
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-02-03 13:54:55 +0100

    iss - done

--- react/data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react/data-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,45 +1,35 @@
-import { useEffect, useState } from "react";
-import Controls from "../Controls/index";
-import Map from "../Map/index";
+import { useEffect, useState } from 'react'
+import Controls from '../Controls/index'
+import Map from '../Map/index'
+import useSWR from 'swr'
+import { mutate } from 'swr'
 
-const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+const URL = 'https://api.wheretheiss.at/v1/satellites/25544'
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+    const { data, error, isLoading, mutate } = useSWR(URL, fetcher, {
+        refreshInterval: 5000,
+    })
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
+    if (error) return <p>Error: {error}</p>
+    if (isLoading) return <p>Loading....</p>
+
+    function handleReload() {
+        mutate(data)
     }
-  }
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
-
-  return (
-    <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
-      <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
-      />
-    </main>
-  );
+    return (
+        <main>
+            <Map
+                longitude={data.longitude}
+                latitude={data.latitude}
+            />
+            <Controls
+                longitude={data.longitude}
+                latitude={data.latitude}
+                onRefresh={handleReload}
+            />
+        </main>
+    )
 }
