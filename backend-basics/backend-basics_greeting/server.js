847a8b6fcd83b689cf1cd56aef4562ecd0bc435b
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-02-06 10:52:34 +0100

    greeting-done

--- backend-basics/backend-basics_greeting/server.js
+++ backend-basics/backend-basics_greeting/server.js
@@ -1,3 +1,27 @@
-import { createServer } from "node:http";
+import { createServer, request } from 'node:http'
 
 // export const server = …
+
+export const server = createServer((request, Response) => {
+    Response.end('Hello, Idan')
+})
+
+// import { readFileSync } from 'node:fs'
+// import { createServer } from 'node:http'
+
+// // similar to element.addEventListener()
+// const server = createServer((request, response) => {
+//     if (request.url === '/api/bad-jokes') {
+//         const data = readFileSync('./data.json')
+//         console.log('data requested')
+//         response.end(data)
+//         return
+//     }
+
+//     const htmlfile = readFileSync('./index.html')
+//     response.end(htmlfile)
+// })
+
+// server.listen(3000, () => {
+//     console.log('server up and running on http://localhost:3000')
+// })
