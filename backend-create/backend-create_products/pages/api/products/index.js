45763a09f8370df500108e9b9ae06a73ff5b4640
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-02-12 09:36:43 +0100

    wip

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -1,15 +1,32 @@
-import dbConnect from "@/db/connect";
-import Product from "@/db/models/Product";
+import dbConnect from '@/db/connect';
+import Product from '@/db/models/Product';
 
 export default async function handler(request, response) {
-  await dbConnect();
+    await dbConnect();
 
-  if (request.method === "GET") {
-    const products = await Product.find();
+    console.log('request:', request);
 
-    response.status(200).json(products);
-    return;
-  }
+    if (request.method === 'GET') {
+        const products = await Product.find();
 
-  response.status(405).json({ status: "Method not allowed." });
+        response.status(200).json(products);
+        return;
+    }
+
+    if (request.method === 'POST') {
+        try {
+            const productData = request.body;
+            console.log('productData:', productData);
+
+            await Product.create(productData);
+
+            response.status(200).json({ status: 'Product was created' });
+
+            return;
+        } catch (error) {
+            response.status(400).json({ error: error.message });
+        }
+    }
+
+    response.status(405).json({ status: 'Method not allowed.' });
 }
